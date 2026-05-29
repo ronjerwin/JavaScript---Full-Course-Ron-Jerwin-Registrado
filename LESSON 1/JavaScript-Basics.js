be4f440117ad5f0c65f6a7e130db0267506314b0
Ron Jerwin Registrado <ronjerwin.dvcode@gmail.com>
2026-05-29 11:47:24 +0800

    Lesson 1: Completed

--- LESSON 1/JavaScript-Basics.js
+++ LESSON 1/JavaScript-Basics.js
@@ -10,3 +10,21 @@ alert("Hello World!"); // This will show a pop-up message with the text "Hello W
 
 //  - Giving istructions to the computer (code),
 //  - The computer follows the instructions (code) to do something (output)
+
+// SYNTAX : the rules of the language, how to write code correctly
+
+document.body.innerHTML = "Hello World!"; 
+// This will change the content of the body of the website to "Hello World!"
+
+// We can do mathematics in JavaScript
+//  5 + 3 = 8
+//  5 - 3 = 2
+//  5 * 3 = 15
+//  5 / 3 = 1.6666666666666667
+
+// In this lesson:
+
+// 1. JavaScript is giving instructions to a computer to do something (output).
+// 2. Wrote our first line of code: alert("Hello World!"); and document.body.innerHTML = "Hello World!";
+// 3. Run our code using the console and see the output in the browser.
+// 4. Learned about the syntax of JavaScript and how to write code correctly.
