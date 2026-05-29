6bec40531a2804a4206094a8bb620a47d51443ca
Ron Jerwin Registrado <ronjerwin.dvcode@gmail.com>
2026-05-29 11:18:21 +0800

    first commit

---
+++ LESSON 1/JavaScript-Basics.js
@@ -0,0 +1,12 @@
+// 3 tools to create websites: HTML, CSS, JavaScript
+
+// HTML : structure of the website, content, text, images, videos, links, etc.
+// CSS : style of the website, colors, fonts, layout, etc.
+// JavaScript : interactivity of the website, animations, user interactions, etc.
+
+alert("Hello World!"); // This will show a pop-up message with the text "Hello World!"
+
+// BASICS OF JAVASCRIPT
+
+//  - Giving istructions to the computer (code),
+//  - The computer follows the instructions (code) to do something (output)
