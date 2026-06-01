d6faee60cd21cdcc57c697ac2ca2783a00a505ab
Ron Jerwin Registrado <ronjerwin.dvcode@gmail.com>
2026-06-01 15:25:00 +0800

    Added Exercises for lesson 3

---
+++ Lesson 3 - Strings/Exercises/exercise.js
@@ -0,0 +1,14 @@
+// Lesson 3 Exercises - Strings
+// Note: do these exercises in the Console (right-click > Inspect > Console)
+// 3a. Create the text 'My name is: ' as a string.
+
+// 3b. Create your name as a string (for example: 'Simon').
+
+// 3c. Using concatenation, add the 2 strings from 3a and 3b together
+// to create the text: 'My name is: _' (replace _ with your name).
+
+// 3d. At a restaurant, you order 1 coffee ($5) and 1 bagel ($3). Using math,
+// calculate the total cost, and using concatenation, create the text:
+// 'Total cost: $ __ ' (replace __ with the total you calculated).
+
+// 3e. Do the same thing as 3d, but use a template string and interpolation.
